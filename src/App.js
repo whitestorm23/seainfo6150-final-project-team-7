5a8e614f42d845d3d962f02470422bbc00c8fe9f
TianYin-y <55065711+TianYin-y@users.noreply.github.com>
2019-12-03 23:39:27 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Navigator from "./Navigator/Nav.jsx";
 import About from "./Support/AboutUs.jsx";
 import Contact from "./Support/ContactUs.jsx";
 import Footer from "./Footer/Foo.jsx"
+import kunkka from "./Heroes/kunkka.jsx";
+
 
 
 const externalContent = {
@@ -31,6 +33,7 @@ function App() {
                 <Route path="/" exact component={Home} />
                 <Route path="/NewsPage" exact component={NewsPage} />
                 <Route path="/Heroes" exact component={Heroes} />
+                <Route path="/kunkka" exact component={kunkka} />
                 <Route path="/baz" exact render={() => <Baz content={externalContent} />} />
                 <Route path="/AboutUs" exact component={About} />
                 <Route path="/ContactUs" exact component={Contact} />
